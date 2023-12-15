838d7523fbe74fe10f2d9be7897d780ce191790e
Farhad16 <fhossain389@gmail.com>
2023-12-15 16:45:22 +0600

    Feat: router setup done

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Users from "./pages/users";
+import Dashboard from "./pages/dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Registration />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
