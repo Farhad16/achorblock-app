fbe963435adfc20675eebbebb08388523976a026
Farhad16 <fhossain389@gmail.com>
2023-12-16 23:32:28 +0600

    Feat: axios setup done

---
+++ src/axios/AxiosInstance.tsx
@@ -0,0 +1,22 @@
+// axiosInstance.js
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://reqres.in/",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default instance;
